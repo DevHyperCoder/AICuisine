b8a206896d61f13adc4be2d0bd536b56068a8aca
DevHyperCoder <arduinoleo88@gmail.com>
2021-02-08 10:17:50 +0530

    Add DataBinding library

--- app/src/main/java/com/devhypercoder/aicuisine/ui/main/MainFragment.kt
+++ app/src/main/java/com/devhypercoder/aicuisine/ui/main/MainFragment.kt
@@ -4,25 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.devhypercoder.aicuisine.R
+import com.devhypercoder.aicuisine.databinding.MainFragmentBinding
 import com.devhypercoder.aicuisine.ui.UserViewModel
 
 class MainFragment : Fragment() {
   private val userViewModel: UserViewModel by activityViewModels()
-
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    return inflater.inflate(R.layout.main_fragment, container, false)
-  }
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    val text = "Hello ${userViewModel.userName.value}!"
-    view.findViewById<TextView>(R.id.message).text = text
+    val binding: MainFragmentBinding =
+      DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+    userViewModel.userName.observe(viewLifecycleOwner) {
+      binding.userName = it
+    }
+    return binding.root
   }
 }
