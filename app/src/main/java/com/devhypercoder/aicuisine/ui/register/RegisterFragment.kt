300ce89106c2de1bc0f136b8dd72808a801888d7
DevHyperCoder <arduinoleo88@gmail.com>
2021-02-09 09:26:05 +0530

    Add RegisterFragment Add DataBinding Add navigation support Add ability to move between login and register fragments

--- app/src/main/java/com/devhypercoder/aicuisine/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/devhypercoder/aicuisine/ui/register/RegisterFragment.kt
@@ -1,4 +1,4 @@
-package com.devhypercoder.aicuisine.ui.login
+package com.devhypercoder.aicuisine.ui.register
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,27 +9,26 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.devhypercoder.aicuisine.R
-import com.devhypercoder.aicuisine.databinding.LoginFragmentBinding
+import com.devhypercoder.aicuisine.databinding.RegisterFragmentBinding
 import com.devhypercoder.aicuisine.ui.UserViewModel
 
-class LoginFragment : Fragment() {
+class RegisterFragment : Fragment() {
     private val userViewModel: UserViewModel by activityViewModels()
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding: LoginFragmentBinding =
-            DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
+        val binding: RegisterFragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.register_fragment, container, false)
 
-        binding.loginButton.setOnClickListener {
+        binding.registerBtn.setOnClickListener {
             userViewModel.authStatus.value = !userViewModel.authStatus.value!!
             userViewModel.userName.value = "DevHyperCoder"
             findNavController().popBackStack(R.id.mainFragment, false)
         }
 
-        binding.toRegisterTextView.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        binding.toLoginTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
 
         return binding.root
